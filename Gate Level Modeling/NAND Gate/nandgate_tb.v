2821ce09d9cffeae8e82e9e547d691ca9659ac22
Vishal Karagathara <Kargatharavishal@outlook.com>
2023-06-23 12:01:54 +0530

    Create nandgate_tb.v

---
+++ Gate Level Modeling/NAND Gate/nandgate_tb.v
@@ -0,0 +1,19 @@
+module tb;
+//Data type declaration
+reg a,b;
+wire c;
+
+//DUT instantiation
+nandgate a1(.a(a), .b(b), .c(c));
+
+//generate stimuli
+	initial begin
+		a=0; b=0;
+		#1a=0; b=1;
+		#1a=1; b=0;
+		#1a=1; b=1;
+	end
+  initial begin
+    $monitor("the value of a=%d, b=%d, c=%d", a,b,c);
+  end
+endmodule
