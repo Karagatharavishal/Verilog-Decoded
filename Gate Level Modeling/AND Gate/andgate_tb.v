176e00faf344437f37d951665df37458f3a28ad1
Vishal Karagathara <Kargatharavishal@outlook.com>
2023-06-23 11:40:46 +0530

    Created andgate_tb.v file

---
+++ Gate Level Modeling/AND Gate/andgate_tb.v
@@ -0,0 +1,16 @@
+Module tb;
+//Data type declaration
+reg a,b;
+Wire c;
+
+//DUT instantiation
+andgate a1(.a(a), .b(b), .c(c));
+
+//generate stimuli
+	initial begin
+		a=0; b=0;
+		a=0; b=1;
+		a=1; b=0;
+		a=1; b=1;
+	end
+endmodule
