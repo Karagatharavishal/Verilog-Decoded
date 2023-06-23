b6cfe8ceecee7183b058f40edef0c05cbf5fb635
Vishal Karagathara <Kargatharavishal@outlook.com>
2023-06-23 11:44:57 +0530

    Update andgate_tb.v

--- Gate Level Modeling/AND Gate/andgate_tb.v
+++ Gate Level Modeling/AND Gate/andgate_tb.v
@@ -1,7 +1,7 @@
 module tb;
 //Data type declaration
 reg a,b;
-Wire c;
+wire c;
 
 //DUT instantiation
 andgate a1(.a(a), .b(b), .c(c));
@@ -9,8 +9,11 @@ andgate a1(.a(a), .b(b), .c(c));
 //generate stimuli
 	initial begin
 		a=0; b=0;
-		a=0; b=1;
-		a=1; b=0;
-		a=1; b=1;
+		#1a=0; b=1;
+		#1a=1; b=0;
+		#1a=1; b=1;
 	end
+  initial begin
+    $monitor("the value of a=%d, b=%d, c=%d", a,b,c);
+  end
 endmodule
