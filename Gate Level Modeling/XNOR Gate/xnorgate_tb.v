312ce20f5074fbbb6b7d4a13554bf6f2d7b0689a
Vishal Karagathara <Kargatharavishal@outlook.com>
2023-06-23 13:10:39 +0530

    Create xnorgate_tb.v

---
+++ Gate Level Modeling/XNOR Gate/xnorgate_tb.v
@@ -0,0 +1,21 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+//Data type declaration
+reg a,b;
+wire c;
+
+//DUT instantiation
+xnorgate a1(.a(a), .b(b), .c(c));
+
+//generate stimuli
+	initial begin
+		a=0; b=0;
+		#1a=0; b=1;
+		#1a=1; b=0;
+		#1a=1; b=1;
+	end
+  initial begin
+    $monitor("the value of a=%d, b=%d, c=%d", a,b,c);
+  end
+endmodule
